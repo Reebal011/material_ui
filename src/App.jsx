dffc31cf8948914f807dc7803d16fcaa3d32ce9f
Reebal Akram <reebalakram@gmail.com>
2022-07-18 14:53:52 +0500

    First Try

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+} from "@material-ui/core";
+import { PhotoCamera } from "@mui/icons-material";
+import useStyles from "./styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const App = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6"> Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album!
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              MUI was designed with the Roboto font in mind. So be sure to
+              follow these instructions. For instance, via Google Web Fonts
+            </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See Photo
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outline" color="primary">
+                    See Photo
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      MUI was designed with the Roboto font in mind.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary" align="center">
+          MUI was designed with the Roboto font in mind. So be sure to follow
+          these instructions. For instance, via Google Web Fonts:
+        </Typography>
+      </footer>
+    </>
+  );
+};
+
+export default App;
